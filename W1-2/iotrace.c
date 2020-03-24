8c2675d110a09269ed701fbb41e5776da5f9cc83
bconfiden2 <bconfiden2@naver.com>
2020-03-24 10:43:58 +0900

    0324-1

--- W1-2/iotrace.c
+++ W1-2/iotrace.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
+#include <stdlib.h>
 
 int main(void)
 {
-  int fileNum = 0;
+  int sdaFD = 0;
   ssize_t nread = 0;
   char buf[1024];
 
-  /*
-  int lbaValue = 0;
+  //입력
+  int startLBA = 0;
   int size = 0;
   printf("start LBA : ");
-  scanf("%d", &lbaValue);
+  scanf("%d", &startLBA);
   printf("size : ");
   scanf("%d", &size);
-  */
 
-  //file = fopen("Progress.txt", "r");
-  fileNum = open("Progress.txt", O_RDONLY);
-  nread = read(fileNum, buf, 1024);
 
-  //printf("%d \n", fileno(rFile));
-  printf("%d %d\n", fileNum, nread);
 
-  close(fileNum);
+  //처리
+
+  sdaFD = open("/dev/sda", O_RDONLY); // 파일의 fd 번호, sda를 읽기전용으로 열었
+  //nread = read(fileNum, buf, 1024); // 파일의 바이트
+
+
+
+  //출력
+  printf("FD : %d\n", sdaFD);
+
+
+  close(sdaFD);
   return 0;
 }
