62f71ff942cdf174592a2ec9422576959e4fc0ff
bconfiden2 <bconfiden2@naver.com>
2020-03-27 09:47:14 +0900

    0327-1

--- W1-2/iotrace.c
+++ W1-2/iotrace.c
@@ -8,48 +8,47 @@
 
 int main(void)
 {
+  int fd = 0;
   FILE* file = NULL;
   long int devStartLBA = 0;
   long int devEndLBA = 0;
   long int devCurLBA = 0;
   char* buf = NULL;
   int i = 0;
-  //사용자 입력부분
+
   long int startLBA = 0;
   int size = 0;
 
-  //
+  //fd = open("/dev/sda", O_RDONLY);
+
+
+
   file = fopen("/dev/sda", "r");
+
   fseek(file, 0 , SEEK_SET);
   devStartLBA = ftell(file) / SECTOR_SIZE;
   fseek(file, 0, SEEK_END);
   devEndLBA = ftell(file) / SECTOR_SIZE;
-
   printf("/dev/sda : %ld ~ %ld\n", devStartLBA, devEndLBA);
 
+  //printf("start LBA : ");
+  startLBA = 2048;
+  //scanf("%ld", &startLBA);
+  //printf("size : ");
+  //scanf("%d", &size);
+  size = 8;
 
-  //입력
-  printf("start LBA : ");
-  scanf("%ld", &startLBA);
-  printf("size : ");
-  scanf("%d", &size);
+  startLBA = 2048;
+  size = 8;
 
+  buf = (char*)malloc(sizeof(char) * size * SECTOR_SIZE); // 할당
+  fseek(file, startLBA, SEEK_SET); // 지정 섹터로 이동
+  fread(buf, 1, size * SECTOR_SIZE, file); // 섹터를 size개 읽음
 
-  //처리
-  buf = (char*)malloc(sizeof(char) * size); // 내용 저장해줄 공간 생성
-  fseek(file, startLBA, SEEK_SET); // 입력한 lba 만큼 이동
-  devCurLBA = ftell(file);
-  fread(buf, 1, size, file); // 현재 포인터부터 32바이트를 한바이트씩 읽어 buf에 저장
-
-
-  //출력
-  for(i = 0 ; i < size ; i++)
-  {
-    printf("%c / ", buf[i]);
-  }
-  printf("\n");
-
+  free(buf);
 
   fclose(file);
+  
+
   return 0;
 }
