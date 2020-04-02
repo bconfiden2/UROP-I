cb38e668d14e1967408266dd8cb0445cdf4ad82a
bconfiden2 <bconfiden2@naver.com>
2020-04-02 19:58:45 +0900

    0402_1

--- W1-2/iotrace.c
+++ W1-2/iotrace.c
@@ -9,48 +9,31 @@
 int main(void)
 {
   int fd = 0;
-  //FILE* file = NULL;
-  //long int devStartLBA = 0;
-  //long int devEndLBA = 0;
-  //long int devCurLBA = 0;
   char* buf = NULL;
   char* allignBuf = NULL;
   int pos = 0;
-  //int i = 0;
 
   long int startLBA = 0;
   int size = 0;
 
   fd = open("/dev/sda", O_DIRECT | O_RDONLY);
 
-  //file = fopen("/dev/sda", "r");
+  printf("start LBA : ");
+  scanf("%ld", &startLBA);
+  printf("size : ");
+  scanf("%d", &size);
 
-  //fseek(file, 0 , SEEK_SET);
-  //devStartLBA = ftell(file) / SECTOR_SIZE;
-  //fseek(file, 0, SEEK_END);
-  //devEndLBA = ftell(file) / SECTOR_SIZE;
-  //printf("/dev/sda : %ld ~ %ld\n", devStartLBA, devEndLBA);
+  //startLBA = 2048;
+  //size = 8;
 
-  //printf("start LBA : ");
-  //scanf("%ld", &startLBA);
-  //printf("size : ");
-  //scanf("%d", &size);
-
-  startLBA = 2048;
-  size = 8;
-
-  buf = (char*)malloc(sizeof(char) * size * SECTOR_SIZE); // 할당
+  buf = (char*)malloc(sizeof(char) * size * SECTOR_SIZE);
   pos = posix_memalign((void**)&buf, SECTOR_SIZE, SECTOR_SIZE * size);
-  //fseek(file, startLBA, SEEK_SET); // 지정 섹터로 이동
+
   lseek(fd, startLBA * SECTOR_SIZE, SEEK_SET);
-  //fread(buf, 1, size * SECTOR_SIZE, file); // 섹터를 size개 읽음
   read(fd, buf, size * SECTOR_SIZE);
 
   free(buf);
-
-  //fclose(file);
   close(fd);
-
 
   return 0;
 }
