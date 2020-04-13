5b4b4ac3e68a2564596020e18f6d34bb6300a8d3
bconfiden2 <bconfiden2@naver.com>
2020-04-13 15:31:04 +0900

    c

---
+++ W1-3/blkParser.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+  //%c   %4p  %8S  %8n
+  //cpu  pid  lba  size
+
+  //파일 열어서 -> blkOutput.txt
+  //줄마다 읽으면서 lba 뽑아내기
+  //내가 실행시킨 파일의 pid 에 맞는 파일에서만!
+}
---
+++ W1-3/txtReader.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define SECTOR_SIZE 512
+
+int main(void)
+{
+  int fd = 0;
+  char* buf = NULL;
+  int fileSize = 0;
+  int fileIdx = 0;
+  int prevSeek = 0;
+  int curSeek = 0;
+
+  fd = open("txt.txt", O_RDONLY | O_DIRECT);
+  //buf = (char*)malloc(sizeof(char));
+  buf = (char*)malloc(sizeof(char) * SECTOR_SIZE);
+  posix_memalign((void**)&buf, SECTOR_SIZE, SECTOR_SIZE);
+  fileSize = lseek(fd, 0, SEEK_END);
+  lseek(fd, 0 , SEEK_SET);
+
+  while(curSeek < fileSize)
+  {
+    prevSeek = lseek(fd, curSeek ,SEEK_SET);
+    read(fd, buf, SECTOR_SIZE);
+    curSeek = lseek(fd, prevSeek + SECTOR_SIZE, SEEK_SET);
+    if(curSeek >= fileSize)
+    {
+      curSeek = fileSize;
+      curSeek++;
+    }
+    //printf("byte offset %d ~ %d -> LBA ??? ~ ???\n", prevSeek, curSeek - 1);
+  }
+
+  free(buf);
+  close(fd);
+}
