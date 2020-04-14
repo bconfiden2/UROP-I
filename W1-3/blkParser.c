1b019ff118f64dc3e94f0153babedcc4d703c0db
bconfiden2 <bconfiden2@naver.com>
2020-04-14 20:26:45 +0900

    과제3 임시완성

--- W1-3/blkParser.c
+++ W1-3/blkParser.c
@@ -2,67 +2,56 @@
 #include <stdio.h>
 #include <ctype.h>
 
-typedef struct FileStruct
-{
-  int pid;
-  int startLBA;
-  int endLBA;
-} fileStruct;
-
-typedef struct FileNode
-{
-  fileStruct file;
-  fileStruct* pnext;
-} fileNode;
-
-/*
-fileStruct* seekWithPid(int pid, fileNode* curFile)
-{
-  {
-    if(*curFile.file.pid == pid)
-    {
-      return &*curFile.file
-    }
-    else
-    {
-      if(*curFile.pnext)
-      {
-        curFile = *curFile.pnext;
-      }
-    }
-}*/
+#define SECTOR_SIZE 512
 
 int main(void)
 {
   //%2d %2a %4p  %8S %8n
 
-  char buf[31] = {'\0'};
+  FILE* fp_txt = NULL;
+  int txtFileSize = 0;
+  int curTxtPoint = 0;
+  int nextTxtPoint = 0;
+
   FILE* fp = NULL;
+  char buf[31] = {'\0'};
+  char lba[9] = {'\0'};
+
+  int curPoint = 0;
+
   int i = 0;
+  int j = 0;
+
+  fp_txt = fopen("txt.txt", "r");
+  fseek(fp_txt, 0, SEEK_END);
+  txtFileSize = ftell(fp_txt);
 
   fp = fopen("blkOutput.txt", "r");
 
+  printf("----txt.txt File LBA mapping----\n");
   while(fgets(buf, sizeof(buf), fp))
   {
-    for(i = 0 ; i < sizeof(buf) - 2 ; i++)
+    if(buf[28] == '1' && buf[4] == 'D')  // read사이즈가 1이고 issued 되었을때의 경우
     {
-      if(buf[i] == '\n') break;
-    }
-    if(i != sizeof(buf) - 2) break;
-    //if(isdigit(buf[28]) && buf[29] == '\n')
-    //{
-      //for(i = 0 ; i < sizeof(buf) ; i++)
-      //{
-        //printf("%c", buf[i]);
-      //}
-    //}
-    if(buf[4] != 'C')
-    {
-      printf("%s", buf);
+      for(j = 0 ; j < 8 ; j++)
+      {
+        lba[j] = buf[12 + j];  // 해당 LBA 주소 가져오기
+      }
+      if((curPoint + 1) * SECTOR_SIZE < txtFileSize)
+      {
+        curTxtPoint = curPoint * SECTOR_SIZE;
+        nextTxtPoint = curTxtPoint + SECTOR_SIZE - 1;
+      }
+      else
+      {
+        nextTxtPoint = txtFileSize;
+      }
+      printf("byte offset %d ~ %d\t-> LBA %s\n", curTxtPoint, nextTxtPoint,  lba);
+      curPoint++;
     }
   }
 
-
+  fclose(fp_txt);
   fclose(fp);
 
   //파일 열어서 -> blkOutput.txt
